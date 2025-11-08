b6787c2a7e98bcda7c50f07d61954d3b8ea5a575
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2025-11-08 08:45:11 +0100

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles/main.css';
 
 // Type definitions
 interface TokenData {
